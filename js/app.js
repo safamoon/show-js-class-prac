b8a8311e7512479816558cf6d924a76960dc948c
safamoon <123930669+safamoon@users.noreply.github.com>
2023-05-19 22:54:48 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+$(function(){
+
+   
+
+    $('.mySlider').slick({
+        slidesToShow: 1,
+        slidesToScroll:1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows:true,
+        prevArrow:`#banner .leftArrow`,
+        nextArrow:`#banner .rightArrow`,
+        dots:true,
+        centerMode: true,
+        centerPadding: '0px',
+    
+ 
+    });
+
+  
+
+
+})
+
+
